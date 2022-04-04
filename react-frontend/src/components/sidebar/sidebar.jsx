c0408da4f2b2bc3bc854d98e920857f18bb5f374
Loïc Houmard <l456@houmard.com>
2022-04-04 21:26:35 +0200

    Put state of uploader in app and add style

--- react-frontend/src/components/sidebar/sidebar.jsx
+++ react-frontend/src/components/sidebar/sidebar.jsx
@@ -4,21 +4,8 @@ import {FiUpload} from "react-icons/fi"
 import {IoMdSettings} from "react-icons/io"
 import {BsImages} from "react-icons/bs"
 
-export default function Sidebar() {
+export default function Sidebar(props) {
 
-    const [indexActiv, setIndexActiv] = useState(0)
-
-    function handleUpload(){
-        setIndexActiv(0)
-    }
-
-    function handleShow(){
-        setIndexActiv(1)
-    }
-
-    function handleFilter(){
-        setIndexActiv(2)
-    }
 
     return (
     <div className="sidebar">
@@ -27,15 +14,15 @@ export default function Sidebar() {
                 <h3 className="sidebarTitle">Dashboard</h3>
                 <hr className="horizontalLine"/>
                 <ul className="sidebarList">
-                    <li className= {indexActiv===0 ? "sidebarListItem active" : "sidebarListItem"} onClick={handleUpload}>
+                    <li className= {props.indexActiv===0 ? "sidebarListItem active" : "sidebarListItem"} onClick={props.handleUpload}>
                         <FiUpload className="sidebarIcon"/>
                         Upload
                     </li>
-                    <li className={indexActiv===1 ? "sidebarListItem active" : "sidebarListItem"}  onClick={handleShow}>
+                    <li className={props.indexActiv===1 ? "sidebarListItem active" : "sidebarListItem"}  onClick={props.handleShow}>
                         <BsImages className="sidebarIcon"/>
                         Similar Images
                     </li>
-                    <li className={indexActiv===2 ? "sidebarListItem active" : "sidebarListItem"}  onClick={handleFilter}>
+                    <li className={props.indexActiv===2 ? "sidebarListItem active" : "sidebarListItem"}  onClick={props.handleFilter}>
                         <IoMdSettings className="sidebarIcon"/>
                         Filter
                     </li>
