1aafe097627017b1848ed1ae9ce244207f71387d
sommemic <sommemic@student.ethz.ch>
2022-06-08 13:50:22 +0200

    clip radar glyphs so they dont overflow. (Zero-one scaling might be better)

--- react-frontend/src/components/similarImages/similarImages.jsx
+++ react-frontend/src/components/similarImages/similarImages.jsx
@@ -14,22 +14,23 @@ export default function SimilarImages(props) {
 					const data = [
 						{
 							data: {
-								dim0: Math.abs(img["latent_coordinate_0"] - props.latentSpace[0]),
-								dim1: Math.abs(img["latent_coordinate_1"] - props.latentSpace[1]),
-								dim2: Math.abs(img["latent_coordinate_2"] - props.latentSpace[2]),
-								dim3: Math.abs(img["latent_coordinate_3"] - props.latentSpace[3]),
-								dim4: Math.abs(img["latent_coordinate_4"] - props.latentSpace[4]),
-								dim5: Math.abs(img["latent_coordinate_5"] - props.latentSpace[5]),
-								dim6: Math.abs(img["latent_coordinate_6"] - props.latentSpace[6]),
-								dim7: Math.abs(img["latent_coordinate_7"] - props.latentSpace[7]),
-								dim8: Math.abs(img["latent_coordinate_8"] - props.latentSpace[8]),
-								dim9: Math.abs(img["latent_coordinate_9"] - props.latentSpace[9]),
-								dim10: Math.abs(img["latent_coordinate_10"] - props.latentSpace[10]),
-								dim11: Math.abs(img["latent_coordinate_11"] - props.latentSpace[11])
+								dim0: Math.min(1, Math.abs(img["latent_coordinate_0"] - props.latentSpace[0])),
+								dim1: Math.min(1, Math.abs(img["latent_coordinate_1"] - props.latentSpace[1])),
+								dim2: Math.min(1, Math.abs(img["latent_coordinate_2"] - props.latentSpace[2])),
+								dim3: Math.min(1, Math.abs(img["latent_coordinate_3"] - props.latentSpace[3])),
+								dim4: Math.min(1, Math.abs(img["latent_coordinate_4"] - props.latentSpace[4])),
+								dim5: Math.min(1, Math.abs(img["latent_coordinate_5"] - props.latentSpace[5])),
+								dim6: Math.min(1, Math.abs(img["latent_coordinate_6"] - props.latentSpace[6])),
+								dim7: Math.min(1, Math.abs(img["latent_coordinate_7"] - props.latentSpace[7])),
+								dim8: Math.min(1, Math.abs(img["latent_coordinate_8"] - props.latentSpace[8])),
+								dim9: Math.min(1, Math.abs(img["latent_coordinate_9"] - props.latentSpace[9])),
+								dim10: Math.min(1, Math.abs(img["latent_coordinate_10"] - props.latentSpace[10])),
+								dim11: Math.min(1, Math.abs(img["latent_coordinate_11"] - props.latentSpace[11]))
 							},
 							meta: { color: "#104242" }
 						}
 					];
+					
 
 					var captions = {}
 					props.dimensionNames.forEach((el, index) => captions["dim" + index] = el)
